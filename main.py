0a5273d52dd0aef56d2d493853445bdcbe15d3c5
Soham Sen <onerandomghost@gmail.com>
2019-08-19 14:40:36 +0530

    Made Better TM

--- main.py
+++ main.py
@@ -6,24 +6,18 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "/mnt/ofc.wlpizza_storage"
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # Max 100 MB.
-ALLOWED_EXTENSIONS = [
-    'js', 'css', 'html', 'json', 'txt', 'mp4', 'avi', 'flv', 'mov', 'wmv',
-    'png', 'jpg', 'jpeg', 'gif', 'tiff', 'bmp', 'mp3', 'wav', 'flac', 'aac',
-    'ogg', 'alac', 'zip', '7z', 'rar', 'pdf', 'exe']
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', allowed_ext=", ".join(ALLOWED_EXTENSIONS))
+    return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
     fileob = request.files["file"]
     fext = secure_filename(fileob.filename).split('.')[-1].lower()
-    if fext.lower() not in ALLOWED_EXTENSIONS:
-        return "Error: Filename extension not allowed."
     filename = "{}.{}".format(''.join(sample(string.ascii_letters + string.digits, 6)), fext)
     save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
     fileob.save(save_path)
-    return '/files/' + filename
+    return request.url_root + 'files/' + filename
